ba6d2f39c041d39665e7506062f48351c4cd614a
OmegaMiku <qrjnz@yandex.ru>
2021-12-09 23:12:56 +0300

    Home Work 1

---
+++ HomeWork1.java
@@ -0,0 +1,43 @@
+class HomeWork1 {
+    public static void main(String[] args) {
+	    printThreeWords();
+		checkHomework();
+		printColor();
+		compareNumbers();
+    }
+	
+	static void printThreeWords() {
+        System.out.println("Orange");
+		System.out.println("Banana");
+		System.out.println("Apple");
+	}
+	
+	static void checkHomework() {
+		int a = 0;
+		int b = -10;
+		System.out.println("Sum is " + (a + b >= 0? "positive" : "negative"));
+	}
+	
+	static void printColor() {
+		int value = 7;
+	    
+	    if (value <= 0){
+			System.out.println("Red");
+		} else if (value > 0 && value <= 100) {
+			System.out.println("Yellow");
+		} else if (value > 100) {
+			System.out.println("Green");
+		}	
+    }
+	
+	static void compareNumbers() {
+		int a = 101;
+		int b = 100;
+		
+		if (a >= b) {
+			System.out.println("a >= b");
+		} else if (a < b) {
+			System.out.println("a < b");
+		}
+	}
+}	
