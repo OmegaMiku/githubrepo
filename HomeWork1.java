39e12b28cd999381a8a02a51c289694bcde7427c
OmegaMiku <qrjnz@yandex.ru>
2021-12-10 10:27:38 +0300

    UPD: Home Work 1

--- HomeWork1.java
+++ HomeWork1.java
@@ -1,40 +1,38 @@
 class HomeWork1 {
-	public static void main(String[] args) {
-	    printThreeWords();
-		checkHomework();
-		printColor();
-		compareNumbers();
-    }
-	
-	static void printThreeWords() {
-        System.out.println("Orange");
-		System.out.println("Banana");
-		System.out.println("Apple");
-	}
-	
-	static void checkHomework() {
-		int a = 0;
-		int b = -10;
-		System.out.println("Sum is " + (a + b >= 0? "positive" : "negative"));
-	}
-	
-	static void printColor() {
-		int value = 7;
-	    
-	    if (value <= 0){
-			System.out.println("Red");
-		} else if (value > 0 && value <= 100) {
-			System.out.println("Yellow");
-		} else if (value > 100) {
-			System.out.println("Green");
-		}	
-    }
-	
-	static void compareNumbers() {
-		int a = 115;
-		int b = 100;
-		System.out.println(a >= b? "a >= b" : "a < b");
-    }	
+public static void main(String[] args) {
+	printThreeWords();
+	checkHomework();
+	printColor();
+	compareNumbers();
 }
 
+static void printThreeWords() {
+	System.out.println("Orange");
+	System.out.println("Banana");
+	System.out.println("Apple");
+}
+
+static void checkHomework() {
+	int a     = 0;
+	int b     = -10;
+	System.out.println("Sum is " + (a + b >= 0? "positive" : "negative"));
+}
+
+static void printColor() {
+	int value = 7;
 	
+	if (value <= 0){
+		System.out.println("Red");
+	} else if (value > 0 && value <= 100) {
+		System.out.println("Yellow");
+	} else if (value > 100) {
+		System.out.println("Green");
+	}	
+}
+
+static void compareNumbers() {
+	int a     = 115;
+	int b     = 100;
+	System.out.println(a >= b? "a >= b" : "a < b");
+}	
+}
