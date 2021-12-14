5332cfcddd2bbd34659be1543eff84b1206af0d5
OmegaMiku <qrjnz@yandex.ru>
2021-12-15 00:20:06 +0300

    Home Work 3

--- HomeWork3.java
+++ HomeWork3.java
@@ -43,11 +43,11 @@ class HomeWork3 {
 
     static void fillDiagonal() {
         int [][] arr = new int[4][4];
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < arr.length; i++) {
             arr[i][i] = 1;
             arr[i][4 - i - 1] = 1;
         }
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.println(Arrays.toString(arr[i]));
         }
     }
